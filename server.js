7d74c2201d880bdc6f2f67e3904753e2262751d4
Tino Phan <tinophan247@gmail.com>
2023-06-05 18:00:40 +0700

    u

--- server.js
+++ server.js
@@ -3,8 +3,10 @@ const app = express();
 const path = require("path");
 const { sequelize } = require("./models");
 const { rootRouter } = require("./routers");
+const dotenv = require("dotenv").config();
 
 app.use(express.json());
+app.use(dotenv);
 
 //set up static file
 const publicPathDirectory = path.join(__dirname, "./public");
@@ -12,7 +14,7 @@ app.use("/public", express.static(publicPathDirectory));
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
-  res.setHeader("Access-Control-Allow-Origin", "https://voucherhunter.netlify.app");
+  res.setHeader("Access-Control-Allow-Origin", process.env.BASE_URL);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
