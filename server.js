ca6a5b5f7d4d8da4a378b0dfe240a0b2efd50811
Tino Phan <tinophan247@gmail.com>
2023-06-05 18:16:15 +0700

    u

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use("/public", express.static(publicPathDirectory));
 
 app.use(function(req, res, next) {
   res.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type, Accept,Authorization,Origin");
-  res.setHeader("Access-Control-Allow-Origin", "https://voucher-hunter-nubrf8ffd-tinophan247.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://voucher-hunter.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
   res.setHeader("Access-Control-Allow-Credentials", true);
   next();
